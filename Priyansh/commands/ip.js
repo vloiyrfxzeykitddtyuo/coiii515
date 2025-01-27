fb73d91e4fd18cb479e53484a5f808145e3b1642
vloiyrfxzeykitddtyuo <lamia582ejso@gmail.com>
2025-01-27 21:31:01 +0300

    Update ip.js

--- Priyansh/commands/ip.js
+++ Priyansh/commands/ip.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-	name: "ip",	
+	name: "ايدي",	
 	version: "1.0.0", 
 	hasPermssion: 0,
 	credits: "NTKhang",
-	description: "View your ip information or other ip", 
-	commandCategory: "other",
+	description: "عرض معلومات IP الخاص بك أو IP آخر", 
+	commandCategory: "اخرى",
 	usages: "",
 	cooldowns: 5, 
 	dependencies: "",
@@ -14,28 +14,28 @@ module.exports.run = async function({ api, args, event, __GLOBAL }) {
   const timeStart = Date.now();
   
     const axios = require("axios");
-  if (!args[0]) {api.sendMessage("Please enter the ip you want to check",event.threadID, event.messageID);}
+  if (!args[0]) {api.sendMessage("الرجاء إدخال عنوان IP الذي تريد فحصه",event.threadID, event.messageID);}
   else {
 var infoip = (await axios.get(`http://ip-api.com/json/${args.join(' ')}?fields=66846719`)).data;
        if (infoip.status == 'fail')
-         {api.sendMessage(`Error! An error occurred. Please try again later: ${infoip.message}`, event.threadID, event.messageID)}
+         {api.sendMessage(`خطأ! حدث خطأ. يرجى المحاولة مرة أخرى لاحقاً: ${infoip.message}`, event.threadID, event.messageID)}
           else {
             /////////////////
           //////////////////
- api.sendMessage({body:`======${(Date.now()) - timeStart}ms=====
- 🗺️Continent: ${infoip.continent}
-🏳️Nation: ${infoip.country}
-🎊Country Code: ${infoip.countryCode}
-🕋Area: ${infoip.region}
-⛱️Region/State: ${infoip.regionName}
-🏙️City: ${infoip.city}
-🛣️District: ${infoip.district}
-📮ZIP code: ${infoip.zip}
-🧭Latitude: ${infoip.lat}
-🧭Longitude: ${infoip.lon}
-⏱️Timezone: ${infoip.timezone}
-👨‍✈️Organization Name: ${infoip.org}
-💵Currency unit: ${infoip.currency}
+ api.sendMessage({body:`======${(Date.now()) - timeStart} ميلي ثانية=====
+ 🗺️القارة: ${infoip.continent}
+🏳️الدولة: ${infoip.country}
+🎊رمز الدولة: ${infoip.countryCode}
+🕋المنطقة: ${infoip.region}
+⛱️المحافظة/الولاية: ${infoip.regionName}
+🏙️المدينة: ${infoip.city}
+🛣️الحي: ${infoip.district}
+📮الرمز البريدي: ${infoip.zip}
+🧭خط العرض: ${infoip.lat}
+🧭خط الطول: ${infoip.lon}
+⏱️المنطقة الزمنية: ${infoip.timezone}
+👨‍✈️اسم المنظمة: ${infoip.org}
+💵العملة: ${infoip.currency}
 `,location: {
 				latitude: infoip.lat,
 				longitude: infoip.lon,
@@ -45,11 +45,3 @@ var infoip = (await axios.get(`http://ip-api.com/json/${args.join(' ')}?fields=6
         }
     
                   }
-
-  
-  
-  
-  
-  
-  
-  
